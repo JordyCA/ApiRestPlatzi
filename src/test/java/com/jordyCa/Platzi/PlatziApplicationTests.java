215530f33f9aabf118df252084dadba76c95e140
JordyCA <jordycastroavendano@gmail.com>
2024-08-25 15:09:43 -0600

    first commit

---
+++ src/test/java/com/jordyCa/Platzi/PlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordyCa.Platzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
